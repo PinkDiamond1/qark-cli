be3a592c1ece939ed24c4afb657b4819b730d049
PMJ <pmj@pmj.hu>
2021-03-14 16:41:45 +0100

    Use anon fnc in closure instead of main()

--- app/index.js
+++ app/index.js
@@ -7,12 +7,10 @@ const rpcEndpoint = 'https://mainnet.infura.io/v3/690402f68fae43b6a8637913a50b28
 const getInfo = require('./menu/info');
 const startMenu = require('./menu/start');
 
-async function main(){
+(async function (){
     const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
     const wallet = (await walletAccess()).connect(provider);
     const contract = await chooseContract(wallet);
     await getInfo(wallet, contract);
     startMenu(contract);
-}
-
-main();
+})()
