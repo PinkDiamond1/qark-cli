dfa4411a08f731e6c146b0140c7764cab6d78ccb
PMJ <pmj@pmj.hu>
2021-04-29 23:01:27 +0200

    Dynamic provider detection, fallback to default

--- app/provider/index.js
+++ app/provider/index.js
@@ -1,6 +1,16 @@
 const rpc = require('./rpc');
+const ethers = require('ethers');
+
+const providers = [
+    rpc
+];
 
 module.exports = async () => {
-    rpc.detect();
-    return rpc.init();
+    for(const i in providers){
+        const provider = providers[i];
+        if(provider.detect()){
+            return provider.init();
+        }
+    }
+    return new ethers.getDefaultProvider();
 }
